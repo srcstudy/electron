3037bba0a4110cfa2ac5efe9f52761b6f105c0f3
elesos <hnrayer@gmail.com>
2020-05-14 10:03:37 +0800

    first blood

---
+++ electron-quick-start/main.js
@@ -0,0 +1,49 @@
+// Modules to control application life and create native browser window
+
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+   mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    //// 在macOS上，当单击dock图标并且没有其他窗口打开时，
+  // 通常在应用程序中重新创建一个窗口。
+
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  //// 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.也可以拆分成几个文件，然后用 require 导入。
---
+++ first_app/index.js
@@ -0,0 +1,27 @@
+//const electron = require('electron')
+
+
+const {app, BrowserWindow} = require('electron')
+
+function createWindow(){
+    let win = new BrowserWindow({
+        width:800,
+        height:600,
+        webPreferences:{
+            nodeIntegration:true
+        }
+    })
+
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+
+//处理窗口关闭事件
+app.on('window-all-closed', () => {
+
+    
+
+})
